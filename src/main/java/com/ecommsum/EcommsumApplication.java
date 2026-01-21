2a179884cae11095e10814e36da6603796140989
Amardeep Singh <64483790+amardeep25may88@users.noreply.github.com>
2026-01-21 21:55:44 +0530

    Add files via upload

---
+++ src/main/java/com/ecommsum/EcommsumApplication.java
@@ -0,0 +1,13 @@
+package com.ecommsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommsumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommsum/EcommsumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommsum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommsumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
